91bb6ab4959da0392b891f88b6d14f8cdc6d963a
Exfeleon <123541988+Exfeleon@users.noreply.github.com>
2024-12-12 11:50:54 +0100

    I cant add button

--- LagerApp(final)/LagerMenu.Designer.cs
+++ LagerApp(final)/LagerMenu.Designer.cs
@@ -63,14 +63,16 @@
             dataGridViewProdukter = new DataGridView();
             button1Update = new Button();
             buttonExportToCVS = new Button();
+            buttonBack = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridViewProdukter).BeginInit();
             SuspendLayout();
             // 
             // buttonProductSearch
             // 
-            buttonProductSearch.Location = new Point(466, 164);
+            buttonProductSearch.Location = new Point(582, 205);
+            buttonProductSearch.Margin = new Padding(4, 4, 4, 4);
             buttonProductSearch.Name = "buttonProductSearch";
-            buttonProductSearch.Size = new Size(94, 29);
+            buttonProductSearch.Size = new Size(118, 36);
             buttonProductSearch.TabIndex = 1;
             buttonProductSearch.Text = "Søg";
             buttonProductSearch.UseVisualStyleBackColor = true;
@@ -79,74 +81,83 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(587, 77);
+            label1.Location = new Point(734, 96);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(84, 20);
+            label1.Size = new Size(101, 25);
             label1.TabIndex = 2;
             label1.Text = "Søg i Lager";
             // 
             // buttonAdd
             // 
-            buttonAdd.Location = new Point(81, 409);
+            buttonAdd.Location = new Point(156, 506);
+            buttonAdd.Margin = new Padding(4, 4, 4, 4);
             buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(94, 29);
+            buttonAdd.Size = new Size(118, 36);
             buttonAdd.TabIndex = 3;
-            buttonAdd.Text = "Add";
+            buttonAdd.Text = "Tilføj";
             buttonAdd.UseVisualStyleBackColor = true;
             buttonAdd.Click += buttonAdd_Click;
             // 
             // textBoxProID
             // 
-            textBoxProID.Location = new Point(37, 35);
+            textBoxProID.Location = new Point(46, 44);
+            textBoxProID.Margin = new Padding(4, 4, 4, 4);
             textBoxProID.Name = "textBoxProID";
-            textBoxProID.Size = new Size(59, 27);
+            textBoxProID.Size = new Size(73, 31);
             textBoxProID.TabIndex = 4;
             textBoxProID.TextChanged += textBoxNavn_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(43, 9);
+            label2.Location = new Point(54, 11);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(75, 20);
+            label2.Size = new Size(93, 25);
             label2.TabIndex = 5;
             label2.Text = "ProduktID";
             label2.Click += label2_Click;
             // 
             // textBoxNavn
             // 
-            textBoxNavn.Location = new Point(133, 35);
+            textBoxNavn.Location = new Point(166, 44);
+            textBoxNavn.Margin = new Padding(4, 4, 4, 4);
             textBoxNavn.Name = "textBoxNavn";
-            textBoxNavn.Size = new Size(125, 27);
+            textBoxNavn.Size = new Size(155, 31);
             textBoxNavn.TabIndex = 6;
             // 
             // textBoxAntal
             // 
-            textBoxAntal.Location = new Point(37, 200);
+            textBoxAntal.Location = new Point(46, 250);
+            textBoxAntal.Margin = new Padding(4, 4, 4, 4);
             textBoxAntal.Name = "textBoxAntal";
-            textBoxAntal.Size = new Size(57, 27);
+            textBoxAntal.Size = new Size(70, 31);
             textBoxAntal.TabIndex = 7;
             // 
             // textBoxSalgsID
             // 
-            textBoxSalgsID.Location = new Point(37, 92);
+            textBoxSalgsID.Location = new Point(46, 115);
+            textBoxSalgsID.Margin = new Padding(4, 4, 4, 4);
             textBoxSalgsID.Name = "textBoxSalgsID";
-            textBoxSalgsID.Size = new Size(59, 27);
+            textBoxSalgsID.Size = new Size(73, 31);
             textBoxSalgsID.TabIndex = 8;
             // 
             // textBoxMaal
             // 
-            textBoxMaal.Location = new Point(161, 200);
+            textBoxMaal.Location = new Point(201, 250);
+            textBoxMaal.Margin = new Padding(4, 4, 4, 4);
             textBoxMaal.Name = "textBoxMaal";
-            textBoxMaal.Size = new Size(59, 27);
+            textBoxMaal.Size = new Size(73, 31);
             textBoxMaal.TabIndex = 9;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(142, 8);
+            label3.Location = new Point(178, 10);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(43, 20);
+            label3.Size = new Size(53, 25);
             label3.TabIndex = 10;
             label3.Text = "Navn";
             label3.Click += label3_Click;
@@ -154,18 +165,20 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(47, 72);
+            label4.Location = new Point(59, 90);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(59, 20);
+            label4.Size = new Size(72, 25);
             label4.TabIndex = 11;
             label4.Text = "SalgsID";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(43, 177);
+            label5.Location = new Point(54, 221);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(44, 20);
+            label5.Size = new Size(53, 25);
             label5.TabIndex = 12;
             label5.Text = "Antal";
             label5.Click += label5_Click;
@@ -173,91 +186,103 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(169, 175);
+            label6.Location = new Point(211, 219);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(42, 20);
+            label6.Size = new Size(41, 25);
             label6.TabIndex = 13;
-            label6.Text = "Maal";
+            label6.Text = "Mål";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(45, 123);
+            label7.Location = new Point(56, 154);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(47, 20);
+            label7.Size = new Size(54, 25);
             label7.TabIndex = 14;
-            label7.Text = "Vaegt";
+            label7.Text = "Vægt";
             // 
             // textBoxVaegt
             // 
-            textBoxVaegt.Location = new Point(37, 145);
+            textBoxVaegt.Location = new Point(46, 181);
+            textBoxVaegt.Margin = new Padding(4, 4, 4, 4);
             textBoxVaegt.Name = "textBoxVaegt";
-            textBoxVaegt.Size = new Size(59, 27);
+            textBoxVaegt.Size = new Size(73, 31);
             textBoxVaegt.TabIndex = 15;
             textBoxVaegt.TextChanged += textBox2_TextChanged;
             // 
             // textBoxBeskriv
             // 
-            textBoxBeskriv.Location = new Point(133, 92);
+            textBoxBeskriv.Location = new Point(166, 115);
+            textBoxBeskriv.Margin = new Padding(4, 4, 4, 4);
             textBoxBeskriv.Name = "textBoxBeskriv";
-            textBoxBeskriv.Size = new Size(125, 27);
+            textBoxBeskriv.Size = new Size(155, 31);
             textBoxBeskriv.TabIndex = 16;
             // 
             // textBoxDato
             // 
-            textBoxDato.Location = new Point(161, 253);
+            textBoxDato.Location = new Point(201, 316);
+            textBoxDato.Margin = new Padding(4, 4, 4, 4);
             textBoxDato.Name = "textBoxDato";
-            textBoxDato.Size = new Size(59, 27);
+            textBoxDato.Size = new Size(73, 31);
             textBoxDato.TabIndex = 17;
             // 
             // textBoxMinbehold
             // 
-            textBoxMinbehold.Location = new Point(37, 253);
+            textBoxMinbehold.Location = new Point(46, 316);
+            textBoxMinbehold.Margin = new Padding(4, 4, 4, 4);
             textBoxMinbehold.Name = "textBoxMinbehold";
-            textBoxMinbehold.Size = new Size(57, 27);
+            textBoxMinbehold.Size = new Size(70, 31);
             textBoxMinbehold.TabIndex = 18;
             // 
             // textBoxMaxbehold
             // 
-            textBoxMaxbehold.Location = new Point(167, 304);
+            textBoxMaxbehold.Location = new Point(209, 380);
+            textBoxMaxbehold.Margin = new Padding(4, 4, 4, 4);
             textBoxMaxbehold.Name = "textBoxMaxbehold";
-            textBoxMaxbehold.Size = new Size(51, 27);
+            textBoxMaxbehold.Size = new Size(63, 31);
             textBoxMaxbehold.TabIndex = 19;
             // 
             // textBoxMoebeltype
             // 
-            textBoxMoebeltype.Location = new Point(37, 304);
+            textBoxMoebeltype.Location = new Point(46, 380);
+            textBoxMoebeltype.Margin = new Padding(4, 4, 4, 4);
             textBoxMoebeltype.Name = "textBoxMoebeltype";
-            textBoxMoebeltype.Size = new Size(57, 27);
+            textBoxMoebeltype.Size = new Size(70, 31);
             textBoxMoebeltype.TabIndex = 20;
             // 
             // textBoxMaterialer
             // 
-            textBoxMaterialer.Location = new Point(133, 145);
+            textBoxMaterialer.Location = new Point(166, 181);
+            textBoxMaterialer.Margin = new Padding(4, 4, 4, 4);
             textBoxMaterialer.Name = "textBoxMaterialer";
-            textBoxMaterialer.Size = new Size(125, 27);
+            textBoxMaterialer.Size = new Size(155, 31);
             textBoxMaterialer.TabIndex = 21;
             // 
             // textBoxKostpris
             // 
-            textBoxKostpris.Location = new Point(37, 355);
+            textBoxKostpris.Location = new Point(46, 444);
+            textBoxKostpris.Margin = new Padding(4, 4, 4, 4);
             textBoxKostpris.Name = "textBoxKostpris";
-            textBoxKostpris.Size = new Size(51, 27);
+            textBoxKostpris.Size = new Size(63, 31);
             textBoxKostpris.TabIndex = 22;
             // 
             // textBoxSalgspris
             // 
-            textBoxSalgspris.Location = new Point(169, 355);
+            textBoxSalgspris.Location = new Point(211, 444);
+            textBoxSalgspris.Margin = new Padding(4, 4, 4, 4);
             textBoxSalgspris.Name = "textBoxSalgspris";
-            textBoxSalgspris.Size = new Size(51, 27);
+            textBoxSalgspris.Size = new Size(63, 31);
             textBoxSalgspris.TabIndex = 23;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(151, 72);
+            label8.Location = new Point(189, 90);
+            label8.Margin = new Padding(4, 0, 4, 0);
             label8.Name = "label8";
-            label8.Size = new Size(81, 20);
+            label8.Size = new Size(97, 25);
             label8.TabIndex = 24;
             label8.Text = "Beskrivelse";
             label8.Click += label8_Click;
@@ -265,18 +290,20 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(169, 229);
+            label9.Location = new Point(211, 286);
+            label9.Margin = new Padding(4, 0, 4, 0);
             label9.Name = "label9";
-            label9.Size = new Size(42, 20);
+            label9.Size = new Size(51, 25);
             label9.TabIndex = 25;
             label9.Text = "Dato";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(11, 229);
+            label10.Location = new Point(14, 286);
+            label10.Margin = new Padding(4, 0, 4, 0);
             label10.Name = "label10";
-            label10.Size = new Size(110, 20);
+            label10.Size = new Size(133, 25);
             label10.TabIndex = 26;
             label10.Text = "Minbeholdning";
             label10.Click += label10_Click;
@@ -284,27 +311,30 @@
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(133, 281);
+            label11.Location = new Point(166, 351);
+            label11.Margin = new Padding(4, 0, 4, 0);
             label11.Name = "label11";
-            label11.Size = new Size(113, 20);
+            label11.Size = new Size(145, 25);
             label11.TabIndex = 27;
-            label11.Text = "Maxbeholdning";
+            label11.Text = "Maksbeholdning";
             // 
             // label12
             // 
             label12.AutoSize = true;
-            label12.Location = new Point(17, 281);
+            label12.Location = new Point(21, 351);
+            label12.Margin = new Padding(4, 0, 4, 0);
             label12.Name = "label12";
-            label12.Size = new Size(89, 20);
+            label12.Size = new Size(107, 25);
             label12.TabIndex = 28;
             label12.Text = "Moebeltype";
             // 
             // label13
             // 
             label13.AutoSize = true;
-            label13.Location = new Point(149, 123);
+            label13.Location = new Point(186, 154);
+            label13.Margin = new Padding(4, 0, 4, 0);
             label13.Name = "label13";
-            label13.Size = new Size(77, 20);
+            label13.Size = new Size(90, 25);
             label13.TabIndex = 29;
             label13.Text = "Materialer";
             label13.Click += label13_Click;
@@ -312,9 +342,10 @@
             // label14
             // 
             label14.AutoSize = true;
-            label14.Location = new Point(33, 333);
+            label14.Location = new Point(41, 416);
+            label14.Margin = new Padding(4, 0, 4, 0);
             label14.Name = "label14";
-            label14.Size = new Size(62, 20);
+            label14.Size = new Size(76, 25);
             label14.TabIndex = 30;
             label14.Text = "Kostpris";
             label14.Click += label14_Click;
@@ -322,55 +353,69 @@
             // label15
             // 
             label15.AutoSize = true;
-            label15.Location = new Point(158, 332);
+            label15.Location = new Point(198, 415);
+            label15.Margin = new Padding(4, 0, 4, 0);
             label15.Name = "label15";
-            label15.Size = new Size(68, 20);
+            label15.Size = new Size(83, 25);
             label15.TabIndex = 31;
             label15.Text = "Salgspris";
             // 
             // textBoxProduktsøgning
             // 
-            textBoxProduktsøgning.Location = new Point(567, 112);
-            textBoxProduktsøgning.Margin = new Padding(3, 4, 3, 4);
+            textBoxProduktsøgning.Location = new Point(709, 140);
+            textBoxProduktsøgning.Margin = new Padding(4, 5, 4, 5);
             textBoxProduktsøgning.Name = "textBoxProduktsøgning";
-            textBoxProduktsøgning.Size = new Size(114, 27);
+            textBoxProduktsøgning.Size = new Size(142, 31);
             textBoxProduktsøgning.TabIndex = 33;
             // 
             // dataGridViewProdukter
             // 
             dataGridViewProdukter.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewProdukter.Location = new Point(437, 200);
-            dataGridViewProdukter.Margin = new Padding(3, 4, 3, 4);
+            dataGridViewProdukter.Location = new Point(546, 250);
+            dataGridViewProdukter.Margin = new Padding(4, 5, 4, 5);
             dataGridViewProdukter.Name = "dataGridViewProdukter";
             dataGridViewProdukter.RowHeadersWidth = 51;
-            dataGridViewProdukter.Size = new Size(274, 200);
+            dataGridViewProdukter.Size = new Size(342, 250);
             dataGridViewProdukter.TabIndex = 34;
             // 
             // button1Update
             // 
-            button1Update.Location = new Point(192, 410);
+            button1Update.Location = new Point(295, 507);
+            button1Update.Margin = new Padding(4, 4, 4, 4);
             button1Update.Name = "button1Update";
-            button1Update.Size = new Size(94, 29);
+            button1Update.Size = new Size(118, 36);
             button1Update.TabIndex = 35;
-            button1Update.Text = "Update";
+            button1Update.Text = "Opdater";
             button1Update.UseVisualStyleBackColor = true;
             button1Update.Click += button1Update_Click;
             // 
             // buttonExportToCVS
             // 
-            buttonExportToCVS.Location = new Point(587, 164);
+            buttonExportToCVS.Location = new Point(734, 205);
+            buttonExportToCVS.Margin = new Padding(4, 4, 4, 4);
             buttonExportToCVS.Name = "buttonExportToCVS";
-            buttonExportToCVS.Size = new Size(94, 29);
+            buttonExportToCVS.Size = new Size(118, 36);
             buttonExportToCVS.TabIndex = 36;
             buttonExportToCVS.Text = "Export CVS";
             buttonExportToCVS.UseVisualStyleBackColor = true;
             buttonExportToCVS.Click += buttonExportToCVS_Click;
             // 
+            // buttonBack
+            // 
+            buttonBack.Location = new Point(26, 508);
+            buttonBack.Name = "buttonBack";
+            buttonBack.Size = new Size(112, 34);
+            buttonBack.TabIndex = 37;
+            buttonBack.Text = "Tilbage";
+            buttonBack.UseVisualStyleBackColor = true;
+            buttonBack.Click += buttonBack_Click;
+            // 
             // LagerMenu
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
+            ClientSize = new Size(1000, 564);
+            Controls.Add(buttonBack);
             Controls.Add(buttonExportToCVS);
             Controls.Add(button1Update);
             Controls.Add(dataGridViewProdukter);
@@ -406,6 +451,7 @@
             Controls.Add(buttonAdd);
             Controls.Add(label1);
             Controls.Add(buttonProductSearch);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "LagerMenu";
             Text = "Antal";
             ((System.ComponentModel.ISupportInitialize)dataGridViewProdukter).EndInit();
@@ -452,6 +498,7 @@
         private DataGridView dataGridViewProdukter;
         private Button button1Update;
         private Button buttonExportToCVS;
+        private Button buttonBack;
     }
 
 }
